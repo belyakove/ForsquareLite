004da06a77f7f1af136b6c05f8165c3ee54c91f0
Eugene Belyakov <eugen.belyakov@gmail.com>
2019-06-03 13:01:33 +0200

    Cleanup of auto generated code

---
+++ ForsquareLite/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.swift
+//  ForsquareLite
+//
+//  Created by Eugene Belyakov on 03/06/2019.
+//  Copyright © 2019 Eugene Belyakov. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+}
+
